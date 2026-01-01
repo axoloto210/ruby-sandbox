d1301137dbe1f9dd5081237111a5be5818b24132
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2026-01-01 17:03:36 +0900

    テストでsetupを使用

--- ticket-gate/test/gate_test.rb
+++ ticket-gate/test/gate_test.rb
@@ -3,22 +3,25 @@ require_relative '../lib/gate'
 require_relative '../lib/ticket'
 
 class GateTest < Minitest::Test
+
+  def setup
+    @umeda = Gate.new(:umeda)
+    @juso = Gate.new(:juso)
+    @mikuni = Gate.new(:mikuni)
+  end
+
   # 梅田から十三へ160円の切符で移動する
   def test_umeda_to_juso
-    umeda = Gate.new(:umeda)
-    juso = Gate.new(:juso)
 
     ticket = Ticket.new(160)
-    umeda.enter(ticket)
-    assert juso.exit(ticket)
+    @umeda.enter(ticket)
+    assert @juso.exit(ticket)
   end
 
   def test_umeda_to_mikuni_when_fare_is_not_enough
-    umeda = Gate.new(:umeda)
-    mikuni = Gate.new(:mikuni)
 
     ticket = Ticket.new(160)
-    umeda.enter(ticket)
-    refute mikuni.exit(ticket)
+    @umeda.enter(ticket)
+    refute @mikuni.exit(ticket)
   end
 end

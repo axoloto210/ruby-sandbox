e84a16d9de1c564dcbeda689886d960da9244dc1
axoloto210 <142103380+axoloto210@users.noreply.github.com>
2026-01-02 13:10:46 +0900

    add fizz buzz test

--- fizz-buzz/lib/fizzbuzz.rb
+++ fizz-buzz/lib/fizzbuzz.rb
@@ -3,11 +3,11 @@
 def fizz_buzz(num)
   if (num % 15).zero? then 'FizzBuzz'
   elsif (num % 3).zero? then 'Fizz'
-  elsif (num % 5).zero? then 'buzz'
-  else num
+  elsif (num % 5).zero? then 'Buzz'
+  else num.to_s
   end
 end
 
-(1..99).each do |num|
-  puts fizz_buzz(num)
-end
+# (1..99).each do |num|
+#   puts fizz_buzz(num)
+# end
